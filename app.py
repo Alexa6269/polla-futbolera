57b5857e0593966c0702a3fbdd0984f4ec172c08
Alexa6269 <104220095+Alexa6269@users.noreply.github.com>
2026-05-23 21:25:00 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     cur = conn.cursor()
-    # Ejecuta el contenido de tu schema.sql
     with open('schema.sql', 'r') as f:
-        cur.execute(f.read())
+        sql_commands = f.read().split(';') # Divide el archivo por comandos
+        for command in sql_commands:
+            if command.strip():
+                cur.execute(command)
     conn.commit()
     cur.close()
     conn.close()
